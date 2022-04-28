37a9359e3b527f07371d8eb985883a04f2d8a584
russnash <russnash37@gmail.com>
2022-04-28 08:15:27 -0400

    Add project files.

---
+++ RequestLayoutBugTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace RequestLayoutBugTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		// Create enough labels to make the view scrollable, thus showing the performance degradation
+		CreateLabels();
+	}
+
+	private void CreateLabels()
+    {
+		for(int i = 0; i < 50; i++)
+        {
+			Label label = new Label();
+			label.Text = i.ToString();
+			layout.Add(label);
+        }
+    }
+}
+
---
+++ RequestLayoutBugTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RequestLayoutBugTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
